e525274acc403a3571fffcbd81c3f8566cfa284f
ananth <ananthkvn2016@gmail.com>
2022-05-07 17:49:49 +0530

    welcome

---
+++ Shop/Productviewcontroller.swift
@@ -0,0 +1,33 @@
+//
+//  Productviewcontroller.swift
+//  Shop
+//
+//  Created by apple on 07/05/22.
+//
+
+import UIKit
+
+class Productviewcontroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let data : String = "data"
+        let valuesdata : String = "dhana"
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
