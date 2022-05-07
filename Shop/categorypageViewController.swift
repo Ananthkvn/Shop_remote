0b1289051d4ef32d5293dd39a51c756b2d9c100f
ananth <ananthkvn2016@gmail.com>
2022-05-07 18:08:40 +0530

    cc

---
+++ Shop/categorypageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  categorypageViewController.swift
+//  Shop
+//
+//  Created by apple on 07/05/22.
+//
+
+import UIKit
+
+class categorypageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("sampledata")
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
